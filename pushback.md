25f32a4b2f7ae623928b4a4686bc581735db08b5
Spencer Tipping <spencer.tipping@gmail.com>
2019-04-13 06:53:48 -0600

    Nuked old io-select; now time to fix things.

--- pushback.md
+++ pushback.md
@@ -4,7 +4,6 @@ Quick table of contents, starting with the internals:
 - [Internal bits](pushback/bits.md)
 - [JIT compiler](pushback/jit.md)
 - [Process multiplexer](pushback/mux.md)
-- [IO selector](pushback/io-select.md)
 
 Public API:
 
@@ -17,20 +16,3 @@ Specific types of streams:
 - [Callback streams](pushback/callback-stream.md)
 - [File streams](pushback/file-stream.md)
 - [TCP server streams](pushback/tcpserver-stream.md)
-
-
-## Multiplexer
-Pushback is built around a [process multiplexer](pushback/mux.md) that manages a
-series of Perl code functions.
-
-
-## IO
-The [IO selector](pushback/io-select.md) uses `select()` to inform the process
-multiplexer about IO device availability.
-
-
-## Streams
-The [stream API](pushback/stream.md) is a high-level interface to IO and
-multiplexed processes. It uses a [JIT compiler](pushback/jit.md) to produce
-optimized processes that bypass OOP hash lookups, function calls, and other
-inefficiencies that are common in Perl code.
--- pushback.pm
+++ pushback.pm
@@ -315,66 +315,6 @@ sub step
 
   $self;
 }
-#line 3 "pushback/io-select.md"
-package pushback::io_select;
-
-sub new
-{
-  my $class = shift;
-  my $self = bless { rfds => "\0" x 128,
-                     wfds => "\0" x 128,
-                     efds => "\0" x 128,
-                     rbuf => "\0" x 128,
-                     wbuf => "\0" x 128,
-                     ebuf => "\0" x 128,
-                     ra   => "\0" x 128,
-                     wa   => "\0" x 128,
-                     ea   => "\0" x 128 }, $class;
-  $$self{mux} = pushback::mux->new(
-    $$self{ra}, $$self{wa}, $$self{ea}, $$self{ea});
-  $self;
-}
-
-sub mux { shift->{mux} }
-
-sub read
-{
-  my ($self, $file) = @_;
-  vec($$self{rfds}, fileno $file, 1) = 1;
-  vec($$self{efds}, fileno $file, 1) = 1;
-  $self;
-}
-
-sub write
-{
-  my ($self, $file) = @_;
-  vec($$self{wfds}, fileno $file, 1) = 1;
-  vec($$self{efds}, fileno $file, 1) = 1;
-  $self;
-}
-
-sub step
-{
-  my ($self, $timeout) = @_;
-  $$self{rbuf} = $$self{rfds}; $$self{rbuf} ^= $$self{ra};
-  $$self{wbuf} = $$self{wfds}; $$self{wbuf} ^= $$self{wa};
-  $$self{ebuf} = $$self{efds}; $$self{ebuf} ^= $$self{ea};
-
-  return 0 unless $$self{rbuf} =~ /[^\0]/ || $$self{wbuf} =~ /[^\0]/;
-  defined(select $$self{rbuf}, $$self{wbuf}, $$self{ebuf}, $timeout) or die $!;
-
-  $$self{ra} |= $$self{rbuf};
-  $$self{wa} |= $$self{wbuf};
-  $$self{ea} |= $$self{ebuf};
-  $self;
-}
-
-sub loop
-{
-  my $self = shift;
-  $self->step;
-  $self->step while $self->mux->loop;
-}
 #line 6 "pushback/io.md"
 package pushback::io;
 use overload qw/ << add /;
